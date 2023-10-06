4f7dab057b6c97aa5418d396a48b0666dbbaaaf7
Stefan Caspersz <stefan@caspersz.com>
2023-10-07 03:47:33 +1100

    startup files

---
+++ start-dev.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+export FLASK_RUN_PORT=8000
+export FLASK_ENV=development
+export FLASK_APP=pvoutput.py
+export QUERY_TOKEN=blahblah
+export PVOUTPUT_SID=101428
+export COOKIE_TOKEN=1f2946e2fd7d0be6c4295c1ed828f0ff4aec21e89df898f9efbaddbe445c5c7c
+flask run --debug
